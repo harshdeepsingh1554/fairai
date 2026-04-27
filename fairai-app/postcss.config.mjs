cab6b250f3324ccbbca5961196c46dcb26169d45
Harsh Deep Singh <harshdeepsingh1554@gmail.com>
2026-04-27 22:18:09 +0530

    Delete fairai-app directory

--- fairai-app/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    // Tailwind v4 doesn't use the old postcss plugin
-    // but some tools may still read this config
-  },
-};
-
-export default config;
